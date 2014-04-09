c5774cd2954de38c2771f0fc003c62fb5dc65046
Antoine Subit <antoine@subit.fr>
2014-04-09 12:04:04 +0200

    Images + contact form

--- src/Asubit/Bundle/PortfolioBundle/Controller/DefaultController.php
+++ src/Asubit/Bundle/PortfolioBundle/Controller/DefaultController.php
@@ -24,4 +24,39 @@ class DefaultController extends Controller
     {
         return $this->render('AsubitPortfolioBundle:Default:contact.html.twig');
     }
+
+    public function contactEnvoiAction()
+    {
+        if(!isset($_POST['name'])) {
+            $this->get('session')->getFlashBag()->add('error', 'Nom obligatoire.');
+        } else {
+            $name = $_POST['name'];
+        }
+        if(!isset($_POST['email'])) {
+            $this->get('session')->getFlashBag()->add('error', 'Email obligatoire.');
+        } else {
+            $email = $_POST['email'];
+        }
+        if(!isset($_POST['message'])) {
+            $this->get('session')->getFlashBag()->add('error', 'Message obligatoire.');
+        } else {
+            $message = $_POST['message'];
+        }
+        if(!isset($_POST['subject'])) {
+            $subject = "Pas de sujet";
+        } else {
+            $subject = $_POST['subject'];
+        }
+        $sujet = 'Contact Portfolio : '.$name.' - '.$subject.'.';
+        $texte = 'Email : '.$email.'<br>Message : '.$message.'.';
+        mail('antoine@subit.fr', $sujet, $texte);
+        $this->get('session')->getFlashBag()->add('success', 'Votre demande à bien été envoyée!');
+        return $this->render('AsubitPortfolioBundle:Default:contact.html.twig', array(
+            'name' => $name,
+            'email' => $email,
+            'message' => $message,
+            'subject' => $subject,
+            )
+        );
+    }
 }
